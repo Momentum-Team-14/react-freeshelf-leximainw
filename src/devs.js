b47a43a6ffe4e7895ba5d3c8db2ac8d41a503076
Alexis Mainwaring <leximainw@gmail.com>
2022-09-09 14:37:11 -0400

    feat: add devs data

---
+++ src/devs.js
@@ -0,0 +1,11 @@
+const devs = [
+    { name: 'Nathan', expertise: 'React', available: true },
+    { name: 'Aziza', expertise: 'Vue, React', available: false },
+    { name: 'Alexis', expertise: 'Rust', available: true },
+    { name: 'Orlando', expertise: 'React, JavaScript', available: true },
+    { name: 'Gary', expertise: 'Flying, JavaScript', available: false },
+    { name: 'Corey', expertise: 'React, ReactRouter', available: true },
+    { name: 'Jeff', expertise: 'React, Remix', available: true },
+]
+
+export default devs;
